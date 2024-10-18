38cc303708358ee1a4488dde9e29b9309155d79d
Alexandru Susnea <alexandru.susnea@outlook.com>
2024-10-18 11:23:29 +0300

    Terraform file creation

---
+++ Pluralsight/IaC on Azure Deep Dive/07-Terraform/virtual-machine.tf
@@ -0,0 +1,13 @@
+terraform {
+   required_version = "~>1.8.4"
+   required_providers {
+     azurerm = {
+        source = "hashicorp/azurerm"
+        version = "~>3.0"
+     }
+     random = {
+        source = "hashicorp/random"
+        version = "~>3.0"
+     }
+   }
+}
