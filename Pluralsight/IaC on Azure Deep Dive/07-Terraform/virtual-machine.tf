95b8ba9037dc0baebcc88ae8e81d2cd7a0da31f6
Alexandru Susnea <alexandru.susnea@outlook.com>
2024-10-18 13:03:55 +0300

    Terraform template update

--- Pluralsight/IaC on Azure Deep Dive/07-Terraform/virtual-machine.tf
+++ Pluralsight/IaC on Azure Deep Dive/07-Terraform/virtual-machine.tf
@@ -1,50 +1,42 @@
 #############################################################################
 # TERRAFORM CONFIG
 #############################################################################
-
 terraform {
-   required_version = "~>1.8.4"
-   required_providers {
-     azurerm = {
+    required_version = ">=1.8.4" // requiring terraform version 1.8.4 or newer
+    required_providers {
+      azurerm = {
         source = "hashicorp/azurerm"
         version = "~>3.0"
-     }
-     random = {
+      }
+      random = {
         source = "hashicorp/random"
         version = "~>3.0"
-     }
-   }
+      }
+    }
 }
-
 
 
 #############################################################################
 # PROVIDERS
 #############################################################################
-
 provider "azurerm" {
   features {
   }
 }
-
 #############################################################################
 # DATA
 #############################################################################
 
 
-
 #############################################################################
 # VARIABLES
 #############################################################################
-
-variable "location" {
+variable "resourceLocation" {
   default = "westeurope"
 }
-
 variable "resourceGroupName" {
   default = "Test-PS-Bicep-102-product2"
 }
-
 
 
 #############################################################################
@@ -57,9 +49,42 @@ resource "azurerm_subnet" "virtualmachine_subnet" {
   virtual_network_name = "vnet-product2"
   address_prefixes = ["10.11.102.0/24"]
 }
-
-
-
-#############################################################################
-# OUTPUTS
-#############################################################################
+resource "azurerm_network_interface" "virtualmachine_nic" {
+  name = "ps-bicep-terraformVM-NIC"
+  location = var.resourceLocation
+  resource_group_name = var.resourceGroupName
+  ip_configuration {
+    name = "ps-bicep-terraformVM-NIC"
+    subnet_id = azurerm_subnet.virtualmachine_subnet.id   // referencing a previously deployed resource
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+resource "random_password" "password" {   // creating a randomly generated password
+  length = 20
+  min_lower = 1
+  min_upper = 1
+  min_numeric = 1
+  min_special = 1
+  special = true
+}
+resource "azurerm_windows_virtual_machine" "main" {    // create the VM itself
+  name = "aspfa-test-ps-bicep-terraformVM"
+  admin_username = "babasha"
+  admin_password = random_password.password.result
+  location = var.resourceLocation
+  resource_group_name = var.resourceGroupName
+  network_interface_ids = [ azurerm_network_interface.virtualmachine_nic.id ]
+  size = "Standard_B2s"
+  
+  os_disk {
+    name = "ps-bicep-terraformVM_os-disk"
+    caching = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer = "WindowsServer"
+    sku = "2022-datacenter-azure-edition"
+    version = "latest"
+  }
+}
